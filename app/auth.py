57a8ee9c009e5102e926e0e76c8dfb8591efd3aa
Nir Albo <niralbo@gmail.com>
2021-03-25 15:41:56 +0200

    authorization router refactor

--- app/auth.py
+++ app/auth.py
@@ -1,94 +1,76 @@
-import functools
-
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
+    Blueprint, g, request, session
 )
-from werkzeug.security import check_password_hash, generate_password_hash
+from werkzeug.security import check_password_hash
 
-from app.db import get_db
+from .db import get_db
+from .queries import create_user, get_user_by_name, get_user
+from .validators import login_required, already_logged_in
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 
-@bp.route('/register', methods=('GET', 'POST'))
+@bp.route('/register', methods=['POST'])
+@already_logged_in
 def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        db = get_db()
-        error = None
-        
-        if not username:
-            error = 'Username is required.'
-        elif not password:
-            error = 'Password is required.'
-        elif db.execute(
-            'SELECT id FROM user WHERE username = ?', (username)
-        ).fetchone() is not None:
-            error = f'User {username} is already registered.'
-        
-        if error is None:
-            db.execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)',
-                (username, generate_password_hash(password))
-            )
-            db.commit()
-            return redirect(url_for('auth.login'))
-        
-        flash(error)
+    # validate form input
+    required = ('Username', 'Password')
+    missing = [key for key in required if key not in request.form]
+
+    if len(missing) > 0:
+        return f'Missing form keys: {", ".join(missing)}.', 400
     
-    return render_template('auth/register.html')
+    # check for user existence in the db
+    username, password = request.form['Username'], request.form['Password']
+    db = get_db()
+    if get_user_by_name(username, db) is not None:
+        return f'"{username}" is already registered.', 400
+
+    # store input user in the db
+    create_user(username, password, db)
+    return 'User created successfully.', 201
 
 
-@bp.route('/login')
+@bp.route('/login', methods=['POST'])
+@already_logged_in
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        db = get_db()
-        error = None
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username)
-        ).fetchone()
-        
-        if user is None:
-            error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
-            error = 'Incorrect password.'
-        
-        if error is None:
-            session.clear()
-            session['user_id'] = user['id']
-            return redirect(url_for('index'))
-        
-        flash(error)
-        
-    return render_template('auth/login.html')
+    # validate form input
+    required = ('Username', 'Password')
+    missing = [key for key in required if key not in request.form]
+
+    if len(missing) > 0:
+        return f'Missing form keys: {", ".join(missing)}.', 400
+    
+    # handle request
+    username, password = request.form['Username'], request.form['Password']
+    db = get_db()
+    user = get_user_by_name(username, db)
+
+    # validate username and password
+    if user is None:
+        return 'Incorrect username.', 400
+    elif not check_password_hash(user['password'], password):
+        return 'Incorrect password.', 400
+
+    # store user_id in session upon success
+    session.clear()
+    session['user_id'] = user['id']
+    return 'Logged in successfully.'
 
 
 @bp.before_app_request
 def load_logged_in_user():
+    # load logged in user before handling a request
     user_id = session.get('user_id')
     
     if user_id is None:
         g.user = None
     else:
-        g.user = get_db().execute(
-            'SELECT * FROM user WHERE id = ?', (user_id)
-        ).fetchone()
+        g.user = get_user(user_id, get_db())
 
 
 @bp.route('/logout')
+@login_required
 def logout():
     session.clear()
-    return redirect(url_for('index'))
-
-
-def login_required(view):
-    @functools.wraps(view)
-    def wrapped_view(**kwargs):
-        if g.user is None:
-            return redirect(url_for('auth.login'))
-        
-        return view(**kwargs)
-    return wrapped_view
+    return 'Logged out successfully.'
